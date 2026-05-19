e00689eff28825d445eb60ce63da86545a8c1b5a
HillkirkLautaro <hillkirklautaro18@gmail.com>
2026-05-19 18:53:55 -0300

    Y

---
+++ Punto1/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+namespace Ejercicio1
+{
+    class Persona
+    {
+        // Propiedades
+        public string Nombre { get; set; }
+        public int Edad { get; set; }
+
+        // Constructor
+        public Persona(string nombre, int edad)
+        {
+            Nombre = nombre;
+            Edad = edad;
+        }
+
+        // Método
+        public void Saludar()
+        {
+            Console.WriteLine($"Hola, mi nombre es {Nombre} y tengo {Edad} años.");
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Instanciar objetos
+            Persona persona1 = new Persona("Lautaro", 20);
+            Persona persona2 = new Persona("Ana", 25);
+
+            // Mostrar datos
+            persona1.Saludar();
+            persona2.Saludar();
+
+            Console.ReadKey();
+        }
+    }
+}
