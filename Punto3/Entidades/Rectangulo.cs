27c983601b6175b8c2c1e6a71aab29db3c3e4495
HillkirkLautaro <hillkirklautaro18@gmail.com>
2026-05-27 21:48:21 -0300

    Update

---
+++ Punto3/Entidades/Rectangulo.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Punto3.Entidades
+{
+    internal class Rectangulo
+    {
+    }
+}
