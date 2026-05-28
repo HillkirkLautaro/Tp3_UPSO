f0a840b04266462099821fdff1b06e241f299970
HillkirkLautaro <hillkirklautaro18@gmail.com>
2026-05-27 22:28:28 -0300

    Update

---
+++ Punto4/Entidades/CuentaBanco.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics.Eventing.Reader;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Punto4.Entidades
+{
+    internal class CuentaBanco
+    {
+        private double saldo;
+        public double Saldo { get => saldo; }
+
+        public double Depositar(double nuevoSaldo) { saldo += nuevoSaldo; return (double)saldo; }
+        public double Retirar(double retyro) { 
+            if (retyro > saldo) { Console.WriteLine("Retiro mayor que saldo");}
+            else {
+                saldo -= retyro;
+            }
+            return (double) saldo;
+        }
+        public CuentaBanco()
+        {
+            saldo = 199;
+        }
+    }
+}
---
+++ Punto4/Program.cs
@@ -0,0 +1,23 @@
+﻿using Punto4.Entidades;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Punto4
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            CuentaBanco cuenta = new CuentaBanco();
+            cuenta.Depositar(100);
+            cuenta.Retirar(300);
+            cuenta.Retirar(50);
+            Console.WriteLine("Saldo final: " + cuenta.Saldo);
+            Console.ReadKey();
+
+        }
+    }
+}
