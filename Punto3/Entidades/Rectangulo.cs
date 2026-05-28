0efb2963e71a0ccd5bd8bb2d05fc3f5c638cf94a
HillkirkLautaro <hillkirklautaro18@gmail.com>
2026-05-27 22:15:30 -0300

    Update

--- Punto3/Entidades/Rectangulo.cs
+++ Punto3/Entidades/Rectangulo.cs
@@ -6,7 +6,33 @@ using System.Threading.Tasks;
 
 namespace Punto3.Entidades
 {
-    internal class Rectangulo
+    class Rectangulo
     {
+        private double baseRectangulo;
+        private double alturaRectangulo;
+
+        
+
+        public double AlturaRectangulo { get => alturaRectangulo; set => alturaRectangulo = value; }
+        public double BaseRectangulo { get => baseRectangulo; set => baseRectangulo = value; }
+
+        public Rectangulo()
+        {
+        }
+
+        public Rectangulo(double baseRectangulo, double alturaRectangulo)
+        {
+            BaseRectangulo = baseRectangulo;
+            AlturaRectangulo = alturaRectangulo;
+        }
+
+        public double CalcularArea()
+        {
+            return BaseRectangulo * AlturaRectangulo;
+        }
+        public double CalcularPerimetro()
+        {
+            return 2 * (BaseRectangulo + AlturaRectangulo);
+        }
     }
 }
--- Punto3/Program.cs
+++ Punto3/Program.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Punto3.Entidades;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -10,6 +11,8 @@ namespace Punto3
     {
         static void Main(string[] args)
         {
+            Rectangulo rectangulo = new Rectangulo(5,3);
+            Console.WriteLine("El area es: {0}, y el perimetro es {1}",rectangulo.CalcularArea(),rectangulo.CalcularPerimetro());
         }
     }
 }
